20e71c3169735dfac682ac4c40182f62a62474ff
Pashka <51019643+Advancedboy@users.noreply.github.com>
2022-06-08 16:52:43 +0300

    Added project files

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_await
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(DoWork));
+            thread.Start();
+            Thread thread2 = new Thread(new ParameterizedThreadStart(DoWorkParameterized));
+            thread2.Start(int.MaxValue);
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine($"Main.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        
+
+        static void DoWork()
+        {
+            int j = 0;
+            for(int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine($"Do Work.");
+                }
+            }
+        }
+        static void DoWorkParameterized(object value)
+        {
+            int j = 0;
+            for (int i = 0; i < (int)value; i++)
+            {
+                j++;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine($"Parameterized.");
+                }
+            }
+        }
+    }
+}
